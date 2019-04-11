733742cf18f0b40cfe76d073692a3bd32ed402a1
Andrewrowe1 <andrew_rowe1@yahoo.co.uk>
2019-04-11 16:25:24 +0100

    base setup from lecture

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  //old
+  // constructor() {
+  //   this.state = {}
+  // }
+
+  //new!
+  state = {
+    tasks: ["sleep", "make fend flapjacks"]
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Title />
+        <TaskAdder addTask={this.addTask} />
+        <List tasks={this.state.tasks} removeTask={this.removeTask} />
       </div>
     );
   }
+
+  removeTask = taskToDelete => {
+    //no-no - this is MUTATION :(
+    // this.state.tasks = this.state.tasks.filter(task => task !== taskToDelete)
+
+    //yes-yes - this is avoids mutation and is best practice
+    this.setState(prevState => {
+      const newTasks = prevState.tasks.filter(task => task !== taskToDelete);
+      return { tasks: newTasks };
+    });
+  };
+
+  addTask = taskToAdd => {
+    this.setState(prevState => {
+      const newArray = [...prevState.tasks, taskToAdd];
+      return { tasks: newArray };
+    });
+  };
+}
+
+function Title() {
+  return <h1 id="heading">My to-do list</h1>;
+}
+
+class TaskAdder extends Component {
+  state = {
+    input: ""
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.handleTyping}
+          type="text"
+          value={this.state.input}
+        />
+      </form>
+    );
+  }
+
+  handleTyping = changeEvent => {
+    const userInput = changeEvent.target.value;
+    this.setState({ input: userInput });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTask(this.state.input);
+    this.setState({ input: "" });
+  };
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.tasks.map(task => {
+        return (
+          <li onClick={event => props.removeTask(task)} key={task}>
+            {task}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default App;
